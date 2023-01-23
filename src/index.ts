1601325ea2ccbd77093bf24a10163505a36d9af2
nopears <gregorzhilich@gmail.com>
2023-01-23 14:55:30 +0300

    Starting and exit program handling

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import {handle} from './handlers/commandHandler'
 
 const ws = new WebSocketServer({ port: 8080 })
 
+console.log(`Server started on ws://localhost:${ws.options.port}/`)
+
 ws.on('connection',(socket) => {
 
   socket.on('message',async (data) => {
     await handle(socket, data.toString())
   })
+})
+
+process.on('SIGINT', () => {
+  ws.close()
+
+  console.log('Program finished')
 })
